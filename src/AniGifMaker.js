fc103d2829f4d515ac6028604df7f2abd8a23d37
Johnston <jcgh00@okdaily.com>
2017-12-23 16:34:26 -0800

    v0.137 prepCanvas

--- src/AniGifMaker.js
+++ src/AniGifMaker.js
@@ -1,8 +1,9 @@
 //This class was designed to work in the CETB environment. Modify it to work as standalone.
 
-//12-23-2017 v0.136 ctx.canvas
+//12-23-2017 v0.137 prepCanvas
 AniGifMaker=function(){
-  this.dim= {w:320, h:160, d:150, sep:'\n'}  //d in seconds
+  this.dim= {w:300, h:120, d:1200, sep:'\n'}  //d in seconds
+  this.dim.colors= ['blue','black','red','white'];
   this.setDim= function(k,v){ this.dim[k]=v }
   const $t= this;
   this.start= function(s){
@@ -12,21 +13,28 @@ AniGifMaker=function(){
     ag.setDelay(dim.d);
     const ian = D.body.appendChild(D.createElement('img'));
 
+    const ctx= $t.prepCanvas();
+    s.split(dim.sep).map( function(x,i){
+      ag.addFrame($t.canvasWk(ctx,x,i));
+    });
+    ag.getBase64GIF(function(image){ ian.src = image; });
+  }
+  this.prepCanvas= function(){
     const cnv= D.createElement('canvas');
-    cnv.width= dim.w; cnv.height= dim.h;
+    cnv.width= $t.dim.w; cnv.height= $t.dim.h;
     const ctx= cnv.getContext('2d');
     ctx.fillStyle= 'yellow';
     ctx.textAlign= 'center';
     ctx.font= "24px Arial";
-    s.split(dim.sep).map( function(x){
-      ag.addFrame($t.canvasWk(ctx,x));
-      ag.setDelay(dim.d);
-    });
-    ag.getBase64GIF(function(image){ ian.src = image; });
+    return ctx;
   }
-  this.canvasWk= function( ctx, x ){
-      ctx.clearRect(0, 0, this.dim.w, this.dim.h);
-      ctx.fillText(x, this.dim.w/2, this.dim.h/2);      
+  this.canvasWk= function( ctx, x, i){
+      const n= $t.dim.colors.length-1;
+      ctx.clearRect(0, 0, $t.dim.w, $t.dim.h);
+      ctx.fillStyle= $t.dim.colors[n];
+      ctx.fillRect(0, 0, $t.dim.w, $t.dim.h);
+      ctx.fillStyle= $t.dim.colors[i % n];
+      ctx.fillText(x, $t.dim.w/2, $t.dim.h/2); 
       const img= new Image();
       return [img,img.src= ctx.canvas.toDataURL('image/png')][0];
   }
