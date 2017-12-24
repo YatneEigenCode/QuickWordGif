1f5c75c5e80b87768fb65bdfd2782d994539fe8a
Johnston <jcgh00@okdaily.com>
2017-12-23 16:05:22 -0800

    v0.134 $t

--- src/AniGifMaker.js
+++ src/AniGifMaker.js
@@ -1,9 +1,10 @@
 //This class was designed to work in the CETB environment. Modify it to work as standalone.
 
-//12-23-2017 v0.132 canvasWk
+//12-23-2017 v0.134 $t
 AniGifMaker=function(){
   this.dim= {w:320, h:160, d:150, sep:'\n'}  //d in seconds
   this.setDim= function(k,v){ this.dim[k]=v }
+  const $t= this;
   this.start= function(s){
     const D= document;
     const ag = new Animated_GIF({repeat:null}), dim=this.dim;
@@ -18,7 +19,7 @@ AniGifMaker=function(){
     ctx.textAlign= 'center';
     ctx.font= "24px Arial";
     s.split(dim.sep).map( function(x){
-      ag.addFrame(this.canvasWk(ctx,x));
+      ag.addFrame($t.canvasWk(ctx,x));
       ag.setDelay(dim.d);
     });
     ag.getBase64GIF(function(image){ ian.src = image; });
